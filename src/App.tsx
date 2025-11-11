c1e7fb39fcfcd20b945ad1c0ea4ec4e45ef3b3fc
fabiii-20 <fabishereef@gmail.com>
2025-11-11 20:36:15 +0530

    minor UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AuthPage from "./pages/AuthPage";
-import Login from "./pages/Login";
 import KeywordSearchPage from "./pages/KeywordSearch";
 import AdminHome from "./pages/AdminDashboard";
 
@@ -10,7 +9,7 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<AuthPage />} />
-        <Route path="/login" element={<Login />} />
+       
         <Route path="/search" element={<KeywordSearchPage />} />
         <Route path="/admin" element={<AdminHome />} />
       </Routes>
